c7b8353c131e9b190946f949650b9c7d92032947
shooter <shooter.hero@gmail.com>
2019-05-24 18:17:07 +0800

    gen ckb testnet addr

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/liushooter/ckb-wallet-golang
 
 go 1.12
 
-require github.com/liushooter/blake2b v1.0.1 // indirect
+require (
+	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d // indirect
+	github.com/liushooter/blake2b v1.0.1 // indirect
+)
--- wallet.go
+++ wallet.go
@@ -3,18 +3,24 @@ package main
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
-	"crypto/rand"
+	crand "crypto/rand"
 	"encoding/hex"
 	"fmt"
 
+	"github.com/btcsuite/btcutil/bech32"
 	newblake2b "github.com/liushooter/blake2b"
+)
+
+const (
+	PREFIX_MAINNET string = "ckb"
+	PREFIX_TESTNET string = "ckt"
 )
 
 func main() {
 
-	pubkeyCurve := elliptic.P256() // http://golang.org/pkg/crypto/elliptic/#P256
+	curve := elliptic.P256() // http://golang.org/pkg/crypto/elliptic/#P256
 
-	keyPair, err := ecdsa.GenerateKey(pubkeyCurve, rand.Reader) // this generates a public & private key pair
+	keyPair, err := ecdsa.GenerateKey(curve, crand.Reader) // this generates a public & private key pair
 	if err != nil {
 		panic(err)
 	}
@@ -34,9 +40,35 @@ func main() {
 
 	hexbin, _ := hex.DecodeString(compressionPubKey)
 
-	blake160 := newblake2b.CkbSum256(hexbin)
+	ckbsum := newblake2b.CkbSum256(hexbin)
+	blake160 := ckbsum[:20]
 
-	fmt.Printf("Blake160: 0x%x\n", blake160[:20])
+	fmt.Printf("Blake160: 0x%x\n", blake160)
+
+	testaddr := genCkbAddr(blake160, PREFIX_TESTNET)
+	fmt.Printf("testaddr: %s\n", testaddr)
+
+}
+
+func genCkbAddr(blake160Addr []byte, prefix string) string {
+
+	typebin, _ := hex.DecodeString("01")
+	bin_idx := []byte("P2PH")
+
+	payload := append(typebin, bin_idx...)
+	payload = append(payload, blake160Addr...)
+
+	converted, err := bech32.ConvertBits(payload, 8, 5, true)
+	if err != nil {
+		panic(err)
+	}
+
+	addr, err := bech32.Encode(prefix, converted)
+	if err != nil {
+		panic(err)
+	}
+
+	return addr
 }
 
 // https://www.socketloop.com/tutorials/golang-example-for-ecdsa-elliptic-curve-digital-signature-algorithm-functions
